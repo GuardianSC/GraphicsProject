040d6d099ee1ccb21217ef430e0a740a434091fc
Chris <calutz1125@gmail.com>
2016-08-25 17:27:21 -0700

    Two triangles draw with colors (same for now)

--- RenderUtilities/Window.cpp
+++ RenderUtilities/Window.cpp
@@ -3,7 +3,21 @@
 #include "GLFW\glfw3.h"
 #include "Window.h"
 #include <cstring>
+#include <cstdio>
+#include <cstdlib>
 
+#ifdef _DEBUG
+void APIENTRY GL_errorCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam)
+{
+	fprintf(stderr, "%s\n", message);
+	
+	if (severity == GL_DEBUG_SEVERITY_HIGH)
+	{
+		abort();
+	}
+}
+
+#endif
 
 bool Window::init(int a_width, int a_height, char * a_title)
 {
@@ -19,7 +33,16 @@ bool Window::init(int a_width, int a_height, char * a_title)
 	glewExperimental = true;
 	glewInit();
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Default background color
-	return true;
+
+	#ifdef _DEBUG
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+	glDebugMessageCallback(GL_errorCallback, 0);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, true);
+#endif
+
+return true; 
 }
 
 bool Window::step()
--- TriangleTest/main.cpp
+++ TriangleTest/main.cpp
@@ -9,20 +9,32 @@ int main()
 	Window window;
 	window.init();
 
-	Vertex vert[3] = { {.5, .5, 0,  1},
-					   { .5, 0, .5,  1},
-					   {-.5, .5, -.5, 1} };
-	unsigned tris[3] = { 0, 1, 2 };
+	Vertex vert[6] = { 
+		{ 0,  .5f, 0, 1, 1, 0, 0, 1 },
+		{ .5f, -.5f, 0, 1, 0, 1, 0, 1 },
+	    { -.5f, -.5f, 0, 1, 0, 1, 1, 1 },
 
-	const char vsource[] = "#version 150\n"
-							"in vec4 position;"
-							"void main() {gl_Position = position;}";
+		{ 0,  -.5f, 0, 1, 1, 0, 0, 1 },
+		{ -.5f, .5f, 0, 1, 0, 1, 0, 1 },
+		{ .5f, .5f, 0, 1, 0, 1, 1, 1 },
+	};
 
-	const char fsource[] = "#version 150\n"
-							"out vec4 outColor;"
-							"void main() {outColor = vec4(1.0, 0.0, 0.0, 1.0);}";
+	unsigned tris[6] = { 0, 1, 2, 3, 4, 5 };
 
-	Geometry geometry = makeGeometry(vert, 3, tris, 3);
+	const char vsource[] = 
+		"#version 330\n"
+		"layout(location = 0)in vec4 position;"
+		"layout(location = 1)in vec4 color;"
+		"out vec4 vColor;"
+		"void main() { vColor = color; gl_Position = position; } ";
+
+	const char fsource[] = 
+		"#version 150\n"
+		"in vec4 vColor;"
+		"out vec4 outColor;"
+		"void main () { outColor = vColor; } ";
+
+	Geometry geometry = makeGeometry(vert, 6, tris, 6);
 	Shader shader = makeShader(vsource, fsource);
 	
 	while (window.step())
