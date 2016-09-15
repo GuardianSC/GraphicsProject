8d72f0f7b8776746aff51646fb69c3c0bbb246ce
Chris <calutz1125@gmail.com>
2016-09-15 16:20:13 -0700

    Commit

--- CameraTest/main.cpp
+++ CameraTest/main.cpp
@@ -9,7 +9,7 @@
 #include "Camera.h"
 #include "glObjects.h"
 #include "procGen.h"
- 
+
 // Welcome to the lighting-feature development branch!
 
 int main()
@@ -31,9 +31,9 @@ int main()
 	Texture noise = genNoise(64, 8);
 
 	/// Loading shader(s)
-	gallery.loadShader("LIGHTING", "../res/Shaders/phongVert.txt"   , "../res/Shaders/phongFrag.txt");
-	gallery.loadShader("CAMERA"  , "../res/Shaders/cameraVert.txt"  , "../res/Shaders/cameraFrag.txt");
-	gallery.loadShader("TEXTURE" , "../res/Shaders/textureVert.txt" , "../res/Shaders/textureFrag.txt");
+	gallery.loadShader("LIGHTING", "../res/Shaders/phongVert.txt", "../res/Shaders/phongFrag.txt");
+	gallery.loadShader("CAMERA", "../res/Shaders/cameraVert.txt", "../res/Shaders/cameraFrag.txt");
+	gallery.loadShader("TEXTURE", "../res/Shaders/textureVert.txt", "../res/Shaders/textureFrag.txt");
 
 	/// Loading object(s)
 	//gallery.loadObjectOBJ("SAMUS"  , "../res/Models/samus.obj");
@@ -46,24 +46,24 @@ int main()
 	Texture tarray[] = { loadTexture("../res/textures/soulspear_diffuse.tga"),
 		loadTexture("../res/textures/soulspear_specular.tga"),
 		loadTexture("../res/textures/soulspear_normal.tga") };
-	
+
 	// Current time
 	float ct = 0;
-	
+
 	// Identity matrix
 	float IDENTITY[16] = { 1, 0, 0, 0, // right
-						   0, 1, 0, 0, // up
-						   0, 0, 1, 0, // forward
-						   0, 0, 0, 1 }; // translate
+		0, 1, 0, 0, // up
+		0, 0, 1, 0, // forward
+		0, 0, 0, 1 }; // translate
 
-	glm::mat4 projection, view, model , model1, model2; // Matrices
+	glm::mat4 projection, view, model, model1, model2; // Matrices
 
-	//projection = glm::ortho<float>(-20, 20, -20, 20, -1000, 1000);
+													   //projection = glm::ortho<float>(-20, 20, -20, 20, -1000, 1000);
 	projection = glm::perspective(45.f, 1.f, .1f, 50.f);
-	view	   = glm::lookAt(glm::vec3(5.f, 5.f, 5.f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 1.f, 0.f)); // eye, center, up
-	model	   = glm::translate(glm::vec3(0, 0, 0));//  * glm::rotate(180.f, glm::vec3(0, -1, 0));
-	model1	   = glm::translate(glm::vec3(0, .5f, 0));//  * glm::rotate(180.f, glm::vec3(0, -1, 0));
-	model2     = glm::translate(glm::vec3(0, -.5f, 0));
+	view = glm::lookAt(glm::vec3(5.f, 5.f, 5.f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 1.f, 0.f)); // eye, center, up
+	model = glm::translate(glm::vec3(0, 0, 0));//  * glm::rotate(180.f, glm::vec3(0, -1, 0));
+	model1 = glm::translate(glm::vec3(0, .5f, 0));//  * glm::rotate(180.f, glm::vec3(0, -1, 0));
+	model2 = glm::translate(glm::vec3(0, -.5f, 0));
 
 	camera.jumpTo(glm::vec3(0, 0, 10));
 	camera.lookAt(glm::vec3(0, 0, 0));
@@ -75,8 +75,8 @@ int main()
 		time.update();
 		input.update();
 		ct += time.getDeltaTime();
-		
-		view	   = camera.getView();
+
+		view = camera.getView();
 		projection = camera.getProjection();
 		camera.update(input, time);
 
